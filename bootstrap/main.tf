96e9bb7a56759b600b05c3ef467aec954749c478
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-05-26 10:20:30 +0900

    fix: #1
    user_cli에게 IAM 권한을 줘서 해결함
    
    close#1
    close #1

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -114,58 +114,58 @@ resource "aws_eip" "ci_server_eip" {
 }
 
 # 역할 생성
-# resource "aws_iam_role" "ec2_s3_role" {
-#   name = "jenkins-s3-access-role"
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "jenkins-s3-access-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#         Action = [
-#           "sts:AssumeRole",
-#         ]
-#       }
-#     ]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+        ]
+      }
+    ]
+  })
+}
 
 # 정책 생성
-# resource "aws_iam_policy" "s3_access_policy" {
-#   name        = "jenkins-s3-access-policy"
-#   description = "Allow Jenkins to access S3"
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "jenkins-s3-access-policy"
+  description = "Allow Jenkins to access S3"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Action = [
-#           "s3:PutObject",
-#           "s3:DeleteObject",
-#         ]
-#         Resource = [
-#           "arn:aws:s3:::*"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+        ]
+        Resource = [
+          "arn:aws:s3:::*"
+        ]
+      }
+    ]
+  })
+}
 
 # # 역할 - 정책 연결
-# resource "aws_iam_role_policy_attachment" "ec2_s3_attachment" {
-#   role       = aws_iam_role.ec2_s3_role.name
-#   policy_arn = aws_iam_policy.s3_access_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "ec2_s3_attachment" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
 
-# # 인스턴스 역할 프로파일 생성
-# resource "aws_iam_instance_profile" "jenkins_instance_profile" {
-#   name = "ec2-s3-instance-profile"
-#   role = aws_iam_role.ec2_s3_role.name
-# }
+# 인스턴스 역할 프로파일 생성
+resource "aws_iam_instance_profile" "jenkins_instance_profile" {
+  name = "ec2-s3-instance-profile"
+  role = aws_iam_role.ec2_s3_role.name
+}
 
 # CI(Jenkins)용 EC2
 resource "aws_instance" "ci_server" {
