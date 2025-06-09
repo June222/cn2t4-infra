b08a6d1f1a369138724089101de82c0a7d025533
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-06-09 11:32:08 +0900

    add: 시작, 종료 shell script
    한번에 시작 종료하는 쉘 스크립트 작성

--- operations/destroy.sh
+++ operations/destroy.sh
@@ -1,1 +1,2 @@
+# terraform destroy -target=module.eks.eks_managed_node_groups -auto-approve
 terraform destroy -auto-approve
--- operations/run_operations.sh
+++ operations/run_operations.sh
@@ -1,7 +1,12 @@
-# terraform init
+terraform init
 terraform fmt
 terraform validate
-terraform apply -auto-approve -refresh=false
+terraform plan
+terraform apply -auto-approve
 
+aws eks update-kubeconfig --region ap-northeast-2 --name eks-cluster-test
+
+terraform output -json > output.json
+# kubectl get nodes
 # 인프라 종료
 # terraform destroy -input=false -auto-approve
