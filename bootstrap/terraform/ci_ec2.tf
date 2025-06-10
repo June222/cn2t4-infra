bff70639b284022b1e35c44e3808bffce22b3e39
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-06-10 17:03:21 +0900

    fix: 잘못된 변수명 수정

--- bootstrap/terraform/ci_ec2.tf
+++ bootstrap/terraform/ci_ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ci_server" {
   security_groups             = [aws_security_group.ci_sg.id]
   associate_public_ip_address = false
   key_name                    = aws_key_pair.ci_ssh_key_pair.key_name
-  iam_instance_profile        = aws_iam_instance_profile.jenkins_instance_profile-v2.name
+  iam_instance_profile        = aws_iam_instance_profile.jenkins_instance_profile.name
 
   root_block_device {
     volume_size = 20
