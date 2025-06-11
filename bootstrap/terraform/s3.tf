b82ff0019e7f17d250ddbc5f8ed68e12aaabbe1f
마정빈 <majb0908@gmail.com>
2025-06-11 13:11:34 +0900

    fix: provider 삭제
    provider가 팀원 코드와 통합하는 과정에서 코드가 중복됨
    
    따라서 provider를 한 번만 선언하므로 삭제
    (alias로 provider 생성 할 경우는 예외)
    
    파일 위치변경

--- bootstrap/terraform/s3.tf
+++ bootstrap/terraform/s3.tf
@@ -1,10 +1,6 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 # S3 버킷을 생성
 resource "aws_s3_bucket" "tikklemoa_bucket" {
-  bucket = "tikklemoa-bucket-test"  # 추후 이름만 수정해서 적용 
+  bucket        = "tikklemoa-bucket-test" # 추후 이름만 수정해서 적용 
   force_destroy = true
 
   tags = {
@@ -19,6 +15,6 @@ resource "aws_s3_bucket_public_access_block" "block_public_access" {
 
   block_public_acls       = true
   ignore_public_acls      = true
-  block_public_policy     = true     
-  restrict_public_buckets = true    
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
