3261304ef2ca3ca924e0ee117cd228b4b616ce80
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-06-13 15:34:56 +0900

    feat: output의 출력 변경
    public -> private 으로 변경
    string -> list(string)으로 변경
    
    둘다 클러스터에 등록하기 위함.

--- bootstrap/terraform/outputs.tf
+++ bootstrap/terraform/outputs.tf
@@ -2,8 +2,8 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "subnet_id" {
-  value = module.vpc.public_subnets[0]
+output "private_subnets" {
+  value = module.vpc.private_subnets
 }
 
 output "jenkins_ip" {
